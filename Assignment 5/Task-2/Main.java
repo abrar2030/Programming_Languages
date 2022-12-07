8c5d3e4bd91a5b91dbbdd5128927cddf1546efbd
Ahmed Abrar <102857826+Abrar-Ahmed-3-0@users.noreply.github.com>
2022-12-07 15:52:52 +0100

    Create Main.java

---
+++ Assignment 5/Task-2/Main.java
@@ -0,0 +1,12 @@
+package homework;
+
+public class Main
+{
+    public static void main(String args[])
+    {
+        System.out.println(TelevisionShop.mmax());
+        System.out.println(TelevisionShop.specific(TelevisionShop.LG));
+        System.out.println(TelevisionShop.everyinfo());
+    }
+    
+}
