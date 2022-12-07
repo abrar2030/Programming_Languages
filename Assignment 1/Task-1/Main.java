606a1908301699b11b3e680251d9c70b1f813221
Ahmed Abrar <102857826+Abrar-Ahmed-3-0@users.noreply.github.com>
2022-12-07 15:16:37 +0100

    Create Main.java

---
+++ Assignment 1/Task-1/Main.java
@@ -0,0 +1,29 @@
+/*
+   Create a program which takes its first command-line argument as an integer number, and decides whether this number is a perfect number.
+   In number theory, a natural number is called perfect, if the sum of its positive divisors (excluding the number itself) is equal to the number.
+   (Or, equivalently, the sum of the positive divisors equals to two times the number.) 
+   The first four perfect numbers are: 6, 28, 496 and 8128. For example, 6=1+2+3.
+*/
+
+public class Main {
+	public static void main(String[] args){
+		int n = Integer.parseInt(args[0]);
+		int sum =0;
+
+		for(int i=1;i<n;i++)
+		{
+			if(n%i==0)
+			{
+				sum +=i;
+			}
+		}
+
+		if(sum == n)
+		{
+			System.out.println("The given number is a Perfect Number.");
+		} else {
+			System.out.println("The given number is not a Perfect Number.");
+		}
+	}
+
+}
